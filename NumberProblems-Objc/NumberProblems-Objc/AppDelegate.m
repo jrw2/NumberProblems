371c238923c80139aabec3ec757ae10a8ba575ac
James Westmoreland <w7jr@me.com>
2014-08-28 16:12:33 -0600

    Added code and tested. Floating pint calculations in Swift appear to be broken.

--- NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
+++ NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
@@ -14,13 +14,33 @@
 
 @implementation AppDelegate
             
+static int multiple = 8;
+static int divisor = 6;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     // Insert code here...
     
+    iterateCount(4);
     
     return YES;
 }
 
+void iterateCount(int number) {
+    if (number > 0) {
+        NSLog(@"Iterate %d", number);
+        int m = multiplied(number);
+        NSLog(@"Multiplied %d by %d to be %d", number, multiple, multiplied(number));
+        NSLog(@"Divided %d by %d to be %f", m, divisor, divided(m));
+        iterateCount(--number);
+    }
+}
+
+int multiplied(int number) {
+    return number * multiple;
+}
+
+float divided(int number) {
+    return (float)number / (float)divisor;
+}
 @end
--- NumberProblems-Swift/NumberProblems-Swift/AppDelegate.swift
+++ NumberProblems-Swift/NumberProblems-Swift/AppDelegate.swift
@@ -14,14 +14,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
+    let multiple = 8
+    let divisor = 6
+    
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
 
         // Insert code here...
         
+        iterateCount(4)
         
         return true
     }
 
-
+    func iterateCount(number: Int) {
+        if (number > 0) {
+            NSLog("Iterate \(number)")
+            var m = multiplied(number)
+            NSLog("Multiplied \(number) by \(multiple) to be \(multiplied(number))")
+            NSLog("Divided \(m) by \(divisor) to be \(divided(m))")
+            iterateCount(number-1)
+        }
+    }
+    
+    func multiplied(number: Int) -> Int {
+        return number * multiple
+    }
+    
+    func divided(number: Int) -> Float {
+        return Float(number) / Float(divisor)
+    }
 }
 
